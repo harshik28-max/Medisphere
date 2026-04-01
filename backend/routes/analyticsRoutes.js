0c1ea494a5f80439c58007b40e48315a1f83ade6
SARELLA HARSHIK <s.harshik.8@gmail.com>
2026-04-01 19:58:32 +0530

    Delete backend directory

--- backend/routes/analyticsRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAnalytics } = require('../controllers/analyticsController');
-const { authenticateToken, authorizeDoctor } = require('../middleware/auth');
-
-router.get('/', authenticateToken, authorizeDoctor, getAnalytics);
-
-module.exports = router;
